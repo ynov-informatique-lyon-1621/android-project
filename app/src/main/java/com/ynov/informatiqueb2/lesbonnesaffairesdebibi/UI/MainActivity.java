a7d45fe501d5cd1b5197946d5e60041af1c6ce8d
TheoCib <theophile.cibert@ynov.com>
2018-04-23 16:19:14 +0200

    Push SplashScreen
    SplashScreen

---
+++ app/src/main/java/com/ynov/informatiqueb2/lesbonnesaffairesdebibi/UI/MainActivity.java
@@ -0,0 +1,26 @@
+package com.ynov.informatiqueb2.lesbonnesaffairesdebibi.UI;
+
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.ynov.informatiqueb2.lesbonnesaffairesdebibi.R;
+
+public class MainActivity extends Activity {
+
+
+    private String[] drawerItemsList;
+    private ListView myDrawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+}
+}
