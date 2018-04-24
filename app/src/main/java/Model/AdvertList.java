ff77d9e54254716f9d9c83d04d2acb6e38917eda
baptiste <baptiste.dumont@ynov.com>
2018-04-24 12:24:46 +0200

    Added Activity

---
+++ app/src/main/java/Model/AdvertList.java
@@ -0,0 +1,44 @@
+package Model;
+
+public class AdvertList {
+
+    // Récupération en priver des informations
+    private String nom;
+    private String espece;
+    private String sexe;
+    private String description;
+
+    // Mise en place en public les variables
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getEspece() {
+        return espece;
+    }
+
+    public void setEspece(String espece) {
+        this.espece = espece;
+    }
+
+    public String getSexe() {
+        return sexe;
+    }
+
+    public void setSexe(String sexe) {
+        this.sexe = sexe;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
+
