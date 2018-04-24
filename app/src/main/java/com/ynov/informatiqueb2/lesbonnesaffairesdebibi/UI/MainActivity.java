54e4007aed467674448dab14d3f92aecb630b2b1
TheoCib <theophile.cibert@ynov.com>
2018-04-24 14:59:21 +0200

    pushTableauJson

--- app/src/main/java/com/ynov/informatiqueb2/lesbonnesaffairesdebibi/UI/MainActivity.java
+++ app/src/main/java/com/ynov/informatiqueb2/lesbonnesaffairesdebibi/UI/MainActivity.java
@@ -2,25 +2,155 @@ package com.ynov.informatiqueb2.lesbonnesaffairesdebibi.UI;
 
 import android.app.Activity;
 import android.content.res.Configuration;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.ynov.informatiqueb2.lesbonnesaffairesdebibi.Adapter.AdapterListAnnonce;
+import com.ynov.informatiqueb2.lesbonnesaffairesdebibi.Model.ListAnnonceModel;
 import com.ynov.informatiqueb2.lesbonnesaffairesdebibi.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    private String[] drawerItemsList;
-    private ListView myDrawer;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        spinner = findViewById(R.id.spinner);
+        String[] itemsCat = new String[]{"Categorie", "Vêtement", "Voiture"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, R.layout.support_simple_spinner_dropdown_item, itemsCat) {
+            @Override
+            public boolean isEnabled(int position) {
+                if (position == 0) {
+                    // Disable the first item from Spinner
+                    // First item will be use for hint
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+
+            @Override
+            public View getDropDownView(int position, View convertView,
+                                        ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                if (position == 0) {
+                    // Set the hint text color gray
+                    tv.setTextColor(Color.GRAY);
+                } else {
+                    tv.setTextColor(Color.BLACK);
+                }
+                return view;
+            }
+        };
+        spinner.setAdapter(adapter);
+    }
 
 
-}
+        public  class GetAnnonces extends AsyncTask<String,String,String> {
+
+            @Override
+            protected String doInBackground(String... strings) {
+                HttpURLConnection myWebService = null;
+                String contenu = "";
+                StringBuilder stringBuilder = null;
+                //Nous faisons un try catch pour gérer les exceptions unchecked. C'est à dire les exceptions qui ne sont pas gérées par l'application.
+                try {
+                    //On donne l'URL pour récupérer notre liste JSON.
+                    URL urlWS = new URL("http://thibault01.com:8081/findAnnonces");
+
+                    //On ouvre une connexion
+                    myWebService = (HttpURLConnection)urlWS.openConnection();
+
+                    //on recupère la réponse du serveur
+                    InputStream in = myWebService.getInputStream();
+
+                    //On set un bufferReader pour pouvoir lire les données du serveur
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+                    //On va concaténer le les donnée que l'on recupère
+                    stringBuilder = new StringBuilder();
+
+                    while ((contenu = reader.readLine()) != null){
+                        //tant qu'il y'a des lignes, on les lis et on les concatène
+                        stringBuilder.append(contenu);
+                    }
+
+                    return stringBuilder.toString();
+
+                } catch (Exception e) {
+                    //Gestion des erreurs
+                    e.printStackTrace();
+                }
+                return stringBuilder.toString();
+                //On retourne notre string concaténée
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                try {
+                    //On crée une liste pour notre JSON
+                    JSONArray jsonArray = new JSONArray(s);
+                    List<ListAnnonceModel> listAnnonce = new ArrayList<>();
+                    //Cette boucle va permettre de récupérer tout les objets JSON présent dans sur le serveur
+                    for (int i = 0; i<jsonArray.length(); i++){
+                        JSONObject o = (JSONObject) jsonArray.getJSONObject(i);
+                        ListAnnonceModel list = new ListAnnonceModel();
+                        /*list.setID(o.getString("id"));
+                        list.setNom(o.getString("nom"));
+                        list.setEspece(o.getString("espece"));
+                        list.setSexe(o.getString("sexe"));
+                        list.setDescription(o.getString("description"));
+                        persoList.add(list);*/
+
+                        list.setTitle(o.getString("titre"));
+                        list.setCategorie(o.getString("categorie"));
+                        list.setDate(o.getString("date"));
+                        list.setDescription(o.getString("description"));
+                        list.setPrix(o.getString("prix"));
+                        list.setVendeur(o.getString("vendeur"));
+                    }
+                    //On instancie notre Adapter et lui passe en argument notre liste d'objets
+                    AdapterListAnnonce adapter = new AdapterListAnnonce(MainActivity.this,0,listAnnonce);
+                    //On recupère notre liste sur notre layout
+                    ListView listView = findViewById(R.id.listAnnonce);
+                    //On set notre list à l'adapter
+                    listView.setAdapter(adapter);
+
+                }
+                catch (JSONException e) {
+                    Log.e("Erreur","Pas de données",e);
+                }
+            }
+        }
+
+
+
+
 }
