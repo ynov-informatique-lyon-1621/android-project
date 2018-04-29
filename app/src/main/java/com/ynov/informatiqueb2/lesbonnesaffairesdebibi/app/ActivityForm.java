3af11faec564305ab1afaae41cd04209cf688d2c
baptiste <baptiste.dumont@ynov.com>
2018-04-29 19:27:11 +0200

    Fix Connect and Activity Form

---
+++ app/src/main/java/com/ynov/informatiqueb2/lesbonnesaffairesdebibi/app/ActivityForm.java
@@ -0,0 +1,84 @@
+package com.ynov.informatiqueb2.lesbonnesaffairesdebibi.app;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.ynov.informatiqueb2.lesbonnesaffairesdebibi.R;
+import com.ynov.informatiqueb2.lesbonnesaffairesdebibi.controller.AuthorizationController;
+
+public class ActivityForm extends AppCompatActivity {
+
+        @Override
+        protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+        setTitle("Formulaire de connexion");
+
+        final SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(ActivityForm.this);
+
+        Button submit = findViewById(R.id.Btn_submit);
+        Button reset = findViewById(R.id.Btn_reset);
+
+        final EditText LogUser = findViewById(R.id.IDuser);
+        final EditText LogPass = findViewById(R.id.IDpass);
+        final CheckBox save = findViewById(R.id.IDcheck);
+
+        if (sharedPreferences.getBoolean("save", false)) {
+            save.setChecked(true);
+            LogUser.setText(sharedPreferences.getString("login", ""));
+            LogPass.setText(sharedPreferences.getString("password", ""));
+        }
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String login = LogUser.getText().toString();
+                String password = LogPass.getText().toString();
+
+                if (TextUtils.isEmpty(login)) {
+                    LogUser.setError("Nom d'utilisateur manquant");
+                } else if (TextUtils.isEmpty(password)) {
+                    LogPass.setError("Mot de passe manquant");
+                } else {
+                    Intent intent = new Intent(ActivityForm.this, ActivityDashboard.class);
+
+                    if (save.isChecked()) {
+                        sharedPreferences.edit()
+                                .putBoolean("save", true)
+                                .putString("login", login)
+                                .putString("password", password)
+                                .apply();
+                    } else {
+                        sharedPreferences.edit()
+                                .putBoolean("save", false)
+                                .apply();
+                    }
+
+                    Toast.makeText(ActivityForm.this
+                            , "Connexion Réussite"
+                            , Toast.LENGTH_LONG).show();
+
+                    new AuthorizationController(ActivityForm.this).execute(LogUser.getText().toString(), LogPass.getText().toString());
+                }
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LogUser.setText("");
+                LogPass.setText("");
+            }
+        });
+    }
+    }
---
+++ app/src/main/java/com/ynov/informatiqueb2/lesbonnesaffairesdebibi/controller/AuthorizationController.java
@@ -0,0 +1,75 @@
+package com.ynov.informatiqueb2.lesbonnesaffairesdebibi.controller;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.ynov.informatiqueb2.lesbonnesaffairesdebibi.app.ActivityDashboard;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.ref.WeakReference;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class AuthorizationController extends AsyncTask<String, String, String> {
+
+    private WeakReference<Activity> weakActivity;
+
+    public AuthorizationController(Activity activity) {
+        weakActivity = new WeakReference<>(activity);
+    }
+
+    private StringBuilder stringBuilder;
+
+    @Override
+    protected String doInBackground(String... strings) {
+        HttpURLConnection httpUrlConnection;
+        String contend;
+        try {
+            URL url = new URL("http://139.99.98.119:8080/findAnnonces?" +
+                    "email=" + strings[0] + "&mdp=" + strings[1]);
+
+            httpUrlConnection = (HttpURLConnection) url.openConnection();
+
+            InputStream in = httpUrlConnection.getInputStream();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+            stringBuilder = new StringBuilder();
+
+            while ((contend = reader.readLine()) != null) {
+                stringBuilder.append(contend);
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return stringBuilder.toString();
+    }
+
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        if (s.equals("true")) {
+            //Faire le traitement de la string reçu
+            Intent intent = new Intent(weakActivity.get().getBaseContext(),
+                    ActivityDashboard.class);
+
+            Log.i("START ACTIVITY", "Connexion réussie");
+            weakActivity.get().startActivity(intent);
+
+        } else {
+            Toast.makeText(weakActivity.get().getBaseContext(), "Votre login ou mot de passe ne correspond pas", Toast.LENGTH_LONG).show();
+        }
+
+
+    }
+}
