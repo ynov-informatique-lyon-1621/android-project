ec1e576e567100f26aa329ff7c35ebd66b0bfb62
TheoCib <theophile.cibert@ynov.com>
2018-04-30 17:58:25 +0200

    ecran confirmation post

---
+++ app/src/main/java/com/ynov/informatiqueb2/lesbonnesaffairesdebibi/UI/ConfirmationDeposerAnnonce.java
@@ -0,0 +1,13 @@
+package com.ynov.informatiqueb2.lesbonnesaffairesdebibi.UI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConfirmationDeposerAnnonce extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmation_deposer_annonce);
+    }
+}
