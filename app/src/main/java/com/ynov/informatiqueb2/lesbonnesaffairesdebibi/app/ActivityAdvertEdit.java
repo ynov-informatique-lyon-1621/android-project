2e40338f3e5a98791d9e422d837971974ebcbc37
Alexandra Ramadour <alexandra.ramadour@ynov.com>
2018-05-01 14:03:02 +0200

    ActivityAdvertCreate + ActivityAdvertEdit

---
+++ app/src/main/java/com/ynov/informatiqueb2/lesbonnesaffairesdebibi/app/ActivityAdvertEdit.java
@@ -0,0 +1,15 @@
+package com.ynov.informatiqueb2.lesbonnesaffairesdebibi.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ynov.informatiqueb2.lesbonnesaffairesdebibi.R;
+
+public class ActivityAdvertEdit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advert_edit);
+    }
+}
