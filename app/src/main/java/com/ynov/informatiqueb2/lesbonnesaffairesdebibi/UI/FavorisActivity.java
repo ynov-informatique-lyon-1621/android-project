2bbb523a263e5997e89a1a43b50c12577f71c494
TakamakWasTaken <gautier.nicollet@ynov.com>
2018-05-03 18:29:52 +0200

    commentary Updates et FML les favoris ARGFjfdn gudnf

---
+++ app/src/main/java/com/ynov/informatiqueb2/lesbonnesaffairesdebibi/UI/FavorisActivity.java
@@ -0,0 +1,19 @@
+package com.ynov.informatiqueb2.lesbonnesaffairesdebibi.UI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ynov.informatiqueb2.lesbonnesaffairesdebibi.Adapter.AdapterFavoris;
+import com.ynov.informatiqueb2.lesbonnesaffairesdebibi.R;
+
+public class FavorisActivity extends com.ynov.informatiqueb2.lesbonnesaffairesdebibi.UI.Menu {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favoris);
+
+
+        AdapterFavoris adapter = new AdapterFavoris(FavorisActivity.this,0);
+    }
+}
