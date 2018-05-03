2cc469e629da3d238c52c6a5138a6079e266f653
Jules LAURENT <jules.laurent@ynov.com>
2018-05-03 10:34:00 +0200

    pretty time

--- app/src/main/java/com/ynov/informatiqueb2/lesbonnesaffairesdebibi/utils/DateFormater.java
+++ app/src/main/java/com/ynov/informatiqueb2/lesbonnesaffairesdebibi/utils/DateFormater.java
@@ -1,6 +1,11 @@
 package com.ynov.informatiqueb2.lesbonnesaffairesdebibi.utils;
 
+import android.text.format.DateUtils;
+
+import org.ocpsoft.prettytime.PrettyTime;
+
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
@@ -10,10 +15,17 @@ public class DateFormater {
     public static final SimpleDateFormat ISODateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
 
     public static String  format(Date date)  {
+
+        if(DateUtils.isToday(date.getTime())) {
+            PrettyTime prettyTime = new PrettyTime(Locale.FRENCH);
+            return prettyTime.format(date);
+        } else {
+            return dateFormat.format(date);
+        }
+
 //        if(date.equals(new Date())) {
 //            return "Aujourd'hui à" + hourFormat.format(date);
-//        } else {
-            return dateFormat.format(date);
+//        } else {         return dateFormat.format(date);
 //        }
     }
 }
